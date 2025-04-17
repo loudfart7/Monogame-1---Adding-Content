4bd74bcd80eb7f5edce1629ace9eb7cb04919d0a
Chloe Vandeworp <339639510@GoTVDSB.ca>
2025-04-17 14:38:21 -0400

    full marks needed still

--- Game1.cs
+++ Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
 
 namespace Monogame_1___Adding_Content
 {
@@ -9,7 +10,12 @@ namespace Monogame_1___Adding_Content
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
+        Random generaetor;
+
         Texture2D dinoTexture;
+
+        Texture2D backgroundTexture;
+        Rectangle window;
 
         public Game1()
         {
@@ -36,6 +42,8 @@ namespace Monogame_1___Adding_Content
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+
+            backgroundTexture = Content.Load<Texture2D>("townMap");
             dinoTexture = Content.Load<Texture2D>("dino");
         }
 
@@ -56,7 +64,9 @@ namespace Monogame_1___Adding_Content
             // TODO: Add your drawing code here
             _spriteBatch.Begin();
 
-            _spriteBatch.Draw(dinoTexture, new Vector2(25,25), Color.White);
+            _spriteBatch.Draw(backgroundTexture, new Vector2(0,0), Color.White);
+
+            _spriteBatch.Draw(dinoTexture, new Vector2(25, 25), Color.White);
 
             _spriteBatch.End();
 
